dd6a6c1e7337f0b5ebb2d94d30453a7e6f7cc539
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-10-31 08:40:36 +0700

    update break and continue

--- bin/controlFlow/controlFlow.dart
+++ bin/controlFlow/controlFlow.dart
@@ -1,8 +1,16 @@
 import 'dart:io';
 
 void main() {
+
+  //this part is to try use method in "breakAndCont" class --------------------
+  breakAndCont bNc = new breakAndCont();
+  bNc.testPrimeNumber();
+
+  print('');
+  bNc.continueTest();
+
   //this part is to try use method in "forLoopTest" class --------------------
-  forLoopTest loop = forLoopTest();
+  // forLoopTest loop = forLoopTest();
   // loop.testLoop();
   //
   // print(''); //make a space
@@ -10,9 +18,9 @@ void main() {
   //
   // print('\n'); //make a space
   // loop.doWhileTest();
-
-  print('\n'); //make a space
-  loop.testPattern();
+  //
+  // print('\n'); //make a space
+  // loop.testPattern();
 
   //this part is to try use method in "ifElseCondition" class --------------------
   // ifElseCondition ife = new ifElseCondition();
@@ -25,6 +33,35 @@ void main() {
   // ife.conditionalExpression();
   //
   // print(ife.testBuy());
+}
+
+class breakAndCont {
+  void testPrimeNumber() {
+    // prime number under 100
+    var primeNumbers = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,73,79,83,89,97];
+
+    var searchNumber = 13;
+    print('Bilangan prima di antara 1-100: $searchNumber');
+
+    for (int i = 0; i < primeNumbers.length; i++) {
+      if (searchNumber == primeNumbers[i]) {
+        print('$searchNumber adalah bilangan prima ke-${i + 1}');
+        break; // stop when the searchNumber is found
+      }
+      print('$searchNumber != ${primeNumbers[i]}');
+    }
+  }
+
+  void continueTest() {
+    for (int i = 1; i <= 10; i++) {
+      if (i % 3 == 0) {
+        continue; // continue
+        // (skip and not prints the value that has a remainder = 0
+        // after divided by 3)
+      }
+      print(i); // prints the value that has a remainder when divided by 3
+    }
+  }
 }
 
 class forLoopTest {
