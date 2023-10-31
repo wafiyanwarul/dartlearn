842216c1f415668a4e2c90834ded9f170579c257
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-10-31 08:23:25 +0700

    update for, while, and do while loop

---
+++ bin/controlFlow/controlFlow.dart
@@ -0,0 +1,138 @@
+import 'dart:io';
+
+void main() {
+  //this part is to try use method in "forLoopTest" class --------------------
+  forLoopTest loop = forLoopTest();
+  // loop.testLoop();
+  //
+  // print(''); //make a space
+  // loop.whileTest();
+  //
+  // print('\n'); //make a space
+  // loop.doWhileTest();
+
+  print('\n'); //make a space
+  loop.testPattern();
+
+  //this part is to try use method in "ifElseCondition" class --------------------
+  // ifElseCondition ife = new ifElseCondition();
+  // ife.simpleIf();
+  // ife.tryElse();
+  //
+  // var test = ifElseCondition();
+  // var score = 100;
+  // print('\nYour score: ${test.calculateScore(score)}');
+  // ife.conditionalExpression();
+  //
+  // print(ife.testBuy());
+}
+
+class forLoopTest {
+  void testLoop() {
+    for (int x = 1; x <= 7; x++) {
+      for (int y = 0; y < x; y++) {
+        stdout.write('$x ');
+      }
+      print(' ');
+    }
+  }
+
+  void whileTest() {
+    var x = 1;
+    while (x > 0 && x <= 25) {
+      stdout.write('$x ');
+      x++;
+    }
+  }
+
+  void doWhileTest() {
+    var x = 10;
+    var y = 11;
+    do {
+      stdout.write('$y ');
+      y--;
+    } while (y <= 11 && y > 0);
+  }
+
+  void testPattern() {
+    // for (int x = 0; x <= 10; x++){
+    //   for (int y = 10; y >= 20; y++){
+    //     stdout.write("@ ");
+    //   }
+    //   print('*');
+    // }
+
+    for (int a = 10; a >= 1; a--){
+      for (int b = 10; b >= 11 - a; b--) {
+        stdout.write('# ');
+      }
+      print(' ');
+    }
+
+  }
+}
+
+class ifElseCondition {
+  void simpleIf() {
+    var isRaining = true; // you can change to 'true/false'
+
+    print('Prepare before going to office.');
+    if (isRaining) {
+      print("Oh. It's raining, bring an umbrella.");
+    }
+    print('Going to the office.');
+  }
+
+  void tryElse() {
+    var openHours = 9;
+    var closedHours = 22;
+    var now = 17;
+
+    if (now > openHours && now < closedHours) {
+      print("\nHello, the store is open");
+    } else {
+      print("\nSorry, the store has closed");
+    }
+  }
+
+  String calculateScore(num score) {
+    if (score > 90) {
+      return 'A';
+    } else if (score > 80) {
+      return 'B';
+    } else if (score > 70) {
+      return 'C';
+    } else if (score > 60) {
+      return 'D';
+    } else {
+      return 'E';
+    }
+  }
+
+  void conditionalExpression() {
+    var openHours = 9;
+    var closedHours = 22;
+    var now = 17;
+
+    // condition ? true expression : false expression
+    var shopStatus = now > openHours
+        ? print("\nHello, we're open")
+        : print("\nSorry, we've closed");
+
+    var name;
+    // expression1 ?? expression2
+    var buyer = name ?? print('user');
+  }
+
+  int testBuy() {
+    int oilThatShouldBuy;
+    var egg = 5;
+
+    if (egg > 0) {
+      oilThatShouldBuy = 6;
+    } else {
+      oilThatShouldBuy = 1;
+    }
+    return oilThatShouldBuy;
+  }
+}
